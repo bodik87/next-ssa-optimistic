8ef84da03e81f4a395c6955d658d74b3891103e7
Bogdan <bsshul@gmail.com>
2024-03-10 16:25:17 +0100

    Fix

--- app/items/page.tsx
+++ app/items/page.tsx
@@ -3,16 +3,17 @@ import { getItems } from '@/lib/items'
 import { Suspense } from 'react'
 
 export default async function Items() {
- const { items = [] } = await getItems()
- return (
-  <Suspense fallback={<p className='mt-4'>Loading...</p>}>
-   <ul className='mt-4 flex flex-col gap-1'>
-    {items.sort(
-     (a, b) => (a.title as any > b.title as any) - (a.title as any < b.title as any))
-     .map(el =>
-      <Element key={el.id} item={el} />
-     )}
-   </ul>
-  </Suspense>
- )
+  const { items = [] } = await getItems()
+
+  return (
+    <Suspense fallback={<p className='mt-4'>Loading...</p>}>
+      <ul className='mt-4 flex flex-col gap-1'>
+        {items.sort(
+          (a, b) => (a.title as any > b.title as any) - (a.title as any < b.title as any))
+          .map(el =>
+            <Element key={el.id} item={el} />
+          )}
+      </ul>
+    </Suspense>
+  )
 }
