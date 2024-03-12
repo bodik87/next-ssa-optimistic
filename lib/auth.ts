723c74b85d199ffe72aefa473c849ccd8157c316
Bogdan <bsshul@gmail.com>
2024-03-12 07:20:49 +0100

    Fix

--- lib/auth.ts
+++ lib/auth.ts
@@ -59,7 +59,7 @@ export async function updateSession(request: NextRequest) {
 
   // Refresh the session so it doesn't expire
   const parsed = await decrypt(session);
-  parsed.expires = new Date(Date.now() + 10 * 1000);
+  parsed.expires = new Date(Date.now() + 3600 * 1000 * 24);
   const res = NextResponse.next();
   res.cookies.set({
     name: "session",
